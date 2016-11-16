f452a5bbac58c58f0212cb2eca3ce9a70849a009
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-11-15 23:29:31 -0500

    changed the route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :fears
-  resources :phobia
   resources :tasks
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
