37fd5380ad9eb21c3359f1143b0be6b670fb31fd
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-11-15 23:41:58 -0500

    Fixed the merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root :to =>'welcome#index'
-
-=======
   resources :fears
   resources :tasks
   devise_for :users
->>>>>>> welcome
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
